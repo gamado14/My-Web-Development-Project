488f9bd9bdabd6699b6162edd310ccc16134726b
gamado14 <60581270+gamado14@users.noreply.github.com>
2020-02-27 14:09:22 -0500

    Create challenge2.php

---
+++ challenge2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+For ($x=99; $x >= 0; $x--) {
+
+	echo "$x bottles of beer on the wall, $x bottles of beer";
+	echo "<br  />";
+
+	$y = $x-1;
+	echo "Take one down, pass it around,$y bottles of beer on the wall";
+	echo "<br  />";
+	echo "<br  />";
+
+
+}
+
+?>
+
+</body>
+</html>
