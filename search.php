3869fa38cc97835ef7fcd222e8dc0fa5e87cebc5
gamado14 <60581270+gamado14@users.noreply.github.com>
2020-05-11 20:59:34 -0400

    Update search.php

--- search.php
+++ search.php
@@ -1,78 +1,76 @@
 <?php
 session_start();
+$_SESSION['username'] = 'username';
+$_SESSION['password'] = '';
+$_SESSION['time'] = time();
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-<title>Explore Art and Themes</title>
+<title>Art Thread</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+<meta name="author" content="Gloriana Amador" />
+<meta name="description" content="Art Interpretation" />
+<meta name="keywords" content="Art, museums, digital" />
+<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
 </head>
 <body>
 
 <?php
-require_once 'auth.php';
+include_once 'header.php';
 require_once 'login.php';
-require_once 'functions.php';
 
 if (isset($_GET['submit'])) {
-	if (empty($_GET['name'])) {
+	if (empty($_GET['artist_name'])) {
 		echo "<p>Please fill out all of the form fields!</p>";
 	} else {
 		$conn = new mysqli($hn, $un, $pw, $db);
 		if ($conn->connect_error) die($conn->connect_error);
-		$name = sanitizeMySQL($conn, $_GET['name']);
-		$query = "SELECT * FROM user NATURAL JOIN collection NATURAL JOIN collection_item NATURAL JOIN art NATURAL JOIN content NATURAL JOIN asset WHERE user_id=1";
-		$result = $conn->query($query);
+		#$name = sanitizeMySQL($conn, $_GET['artist_name']);
+		$query = "SELECT * FROM art WHERE artist_name LIKE '%$artist_name%'";
+			$result = $conn->query($query);
 		if (!$result) {
 			die ("Database access failed: " . $conn->error);
 		} else {
 			$rows = $result->num_rows;
 			if ($rows) {
-				echo 
-				"<h1>Results</h1>
+				echo "<h1>Results</h1>
 				<table><tr>
-				
 				<th>art_id</th>
 				<th>title</th>
-				<th>artist_name/th>
+				<th>artist_name</th>
 				<th>date_created</th>
 				<th>image_file</th>
-				<th>theme</th>
-				<th>content_file_name</th>
-				
 				</tr>";
-				
+
 				while ($row = $result->fetch_assoc()) {
 					echo "<tr>";
-					echo "<td>"
-					.$row["art_id"]."</td><td>"
-					.$row["title"]."</td><td>"
-					.$row["artist_name"]."</td><td>"
-					.$row["date_created"]."</td><td>"
-					.$row["theme"]."</td><td>"
-					.$row["content_file_name"]."</td><td>"
-					.$row["image_file"]."</td>";
+					echo "<td>".$row["art_id"]."</td>
+					<td>".$row["title"]."</td>
+					<td>".$row["artist_name"]."</td>
+					<td>".$row["date_created"]."</td>
+					<td>".$row["image_file"]."</td>";
 					echo "</tr>";
 				}
 				echo "</table>";
 			} else {
-				echo "<p>Sorry, there are no results for this query!</p>";
+				echo "<p>No results!</p>";
 			}
 			echo "<h2>Search Again</h2>";
 		}
 	}
 }
 ?>
+
+<!--Search Form-->
 <form method="get" action="search.php">
 	<fieldset>
-		<legend>Search for Art and Themes</legend>
-		<label for="name">Art:</label>
-		<input type="text" name="name" required><br> 
+		<legend>Explore Art & Themes</legend>
+		<input type="text" name="artist_name" required><br> 
 		<input type="submit" name="submit">
 	</fieldset>
 </form>
-
-<?php include_once 'footer.php'; ?>
-
 </body>
 </html>
